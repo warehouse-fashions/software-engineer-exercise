f5c8ba59f19fdd1f55192750e243b710fea5cf67
ddave592 <david.g1591@googlemail.com>
2020-01-29 20:52:30 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WarehouseCarousel from './containers/warehouseCarousel';
+
+import './styles/App.scss';
+
+function App() {
+  return (
+    <WarehouseCarousel />
+  );
+}
+
+export default App;
---
+++ src/containers/warehouseCarousel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WarehouseCarousel = () => {
+    return (
+        <div>
+            hello world
+        </div>
+    )
+}
+
+export default WarehouseCarousel;

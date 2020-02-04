dc27210a49e92c3f85386936c8d39321aeeede37
ddave592 <david.g1591@googlemail.com>
2020-02-04 23:49:09 +0000

    warehouse component

--- src/containers/warehouseCarousel.js
+++ src/containers/warehouseCarousel.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const WarehouseCarousel = () => {
+import ProductItem from '../components/ProductItem';
+import { NextArrow, PrevArrow } from '../components/CarouselArrows';
+
+const WarehouseCarousel = ({ProductData}) => {
+
+    const [isDragging, setIsDragging] = useState(false);
+    const [draggable, setDraggable] = useState(true);
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        draggable: draggable,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        responsive: [
+            {
+                breakpoint: 1140,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 540,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+    const toggleDragging = (value) => setIsDragging(value);
+    const toggleDraggable = (value) => setDraggable(value);
+
     return (
-        <div>
-            hello world
+        <div className='carousel-container'>
+            <h1>We Recommend</h1>
+            <Slider {...settings} >
+                {ProductData.map((item, index) =>
+                    <ProductItem
+                        key={`item${index}-${item.id}`}
+                        data={item}
+                        draggable={draggable}
+                        isDragging={isDragging}
+                        toggleDragging={toggleDragging}
+                        toggleDraggable={toggleDraggable}
+                    />
+                )}
+            </Slider>
         </div>
     )
 }

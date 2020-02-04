707af7192b1e257c1fac6dadd9dcef00ff2af6bd
Bayo Ade <bayo.300@gmail.com>
2020-02-04 13:52:33 +0000

    update

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 let app = document.querySelector('.container')
 app.innerHTML = `
     <h1 class="app-title">we recommend</h1>
-
     `;
+
 
 fetch('data/recommendations.json')
     .then(function(res) {
         console.log('Response:', res)
         return res.json();
     })
-    .then(function(data) {
+
+
+
+.then(function(data) {
             console.log(data.hits)
             let box = document.createElement('div')
             box.classList.add('boxy')
@@ -18,6 +21,8 @@ fetch('data/recommendations.json')
             let innerbox = document.createElement('div')
             innerbox.classList.add('inner-boxy')
             box.appendChild(innerbox)
+
+
 
             innerbox.innerHTML = `${data.hits.map(function(product){ 
             if(product.image){
@@ -30,17 +35,66 @@ fetch('data/recommendations.json')
                 </div>
                 </div>
                 ` 
-            }
-        
+            }        
         }).join('')}`
+        
+        let productsImage = document.addEventListener('click', function(e){
+            if (e.target && e.target.classList=="item-image") {
 
-        let item = document.querySelector('.item');
-        innerbox.appendChild(item);
+                fetch('data/product.json')
+                .then(function(res) {
+                    console.log('Response:', res)
+                    return res.json();
+                })
 
+                .catch(function(error) {
+                    throw new Error(error);
+                })
+
+                .then(function(data2){
+                   const dataArray = Object.values(data2.data)
+                   console.log(dataArray)
+                   dataArray.forEach(function(name){
+                       console.log(name.name)
+                   })
+                })
+                
+            }
+        })
+        
     })
 
+    
 
+    
+    // for(var i = 0; i < data2.data.length; i++){
+    //     if(data.hits[i].product_name === data2.data[i].name){
+    //         console.log(data.hits[i].product_name)
+    //     }
 
+    // }
+  
+//  modal itself 
+    // let modalcontainer = document.createElement('div')
+    // modalcontainer.classList.add('modal-container')
+    // app.appendChild(modalcontainer)
+    // modalcontainer.innerHTML = `
+
+    //     <div class="modal-grid">
+    //     <div class="modal-header">
+
+    //         <div class="modal-title">Example Modal</div>
+    //         <div class="close-button">&times;</div>
+    //     </div>
+
+    //     <div class="modal-body">
+    //     this is where the info for the product will go
+    //     </div>
+
+    //     </div>
+        
+        
+    //     `
 
 
 

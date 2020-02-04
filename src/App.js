7b11de25a080de053394dfd39513d48377535e3e
ddave592 <david.g1591@googlemail.com>
2020-02-04 23:48:13 +0000

    pass through data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import WarehouseCarousel from './containers/warehouseCarousel';
+import { data as ProductData } from './data/product.json';
 
+import './styles/normalize.scss'
 import './styles/App.scss';
 
 function App() {
   return (
-    <WarehouseCarousel />
+        <WarehouseCarousel ProductData={ProductData} />
   );
 }
 

d11c5e5c40691bc842142e58517391a717a5cccb
Agata <agata@swistowski.org>
2020-02-13 16:39:44 +0000

    Remember product id in state and dispaly modal with product_id

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import React, {useState} from 'react';
 import './App.css';
 import recommendations from './data/recommendations'
 
+
 function App() {
 
-    const products = recommendations.hits.map(product => product);
     const [choosenProduct, setChoosenProduct] = useState('');
 
-    function handleCLick(choosenProduct) {
+    function onProductSelected(productId) {
+        // tu znajdż produkt który ma variants z twoim productID i zapamiętaj jako choosenPoduct
         setChoosenProduct(choosenProduct);
-        console.log(choosenProduct)
     }
 
     return (
         <div className="App">
             <h1>We recommend</h1>
-            {products.map((product, i) => product.image ? (
-                <div key={i} onClick={() => handleCLick(product.product_id)}>
+            {recommendations.hits.map((product, i) => product.image ? (
+                <div key={i} onClick={() => setChoosenProduct(product.product_id)}>
                     <img src={product.image.link} alt={product.image.alt} title={product.image.title}/>
                     <p>{product.product_name}</p>
                     <p>£ {product.price}</p>
                 </div>
-                ) : '')}
+            ) : '')}
+            {choosenProduct ?
+                <div className='modal' onClick={() => setChoosenProduct('')}>
+                    <p>{choosenProduct}</p>
+                </div> : ''}
         </div>
     );
 }

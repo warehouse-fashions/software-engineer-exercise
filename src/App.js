eb849e18ad5d0c2d53e12ff4405e48d76a786137
Agata <agata@swistowski.org>
2020-02-14 16:19:15 +0000

    Add reset.css; Add styling; Add prevent default; Refactor code

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function Color({value, image_groups}) {
 
 function Size({name, value}) {
     return (
-        <div>{name}</div>
+        <div className='modal-size-individual'>{name}</div>
     )
 }
 
@@ -39,43 +39,48 @@ function App() {
 
     const setCurrentProduct = (productId) => {
         const newChoosenProduct = getProduct(productId);
-        console.log("setting: ", newChoosenProduct);
         setChoosenProduct(newChoosenProduct);
         setVariationValue(newChoosenProduct.image_groups.filter(({view_type}) => view_type === 'hi-res')[0].variation_value);
     };
 
-
     return (
         <div className="App">
             <h1>We recommend</h1>
-            {recommendations.hits.map((product, i) => product.image ? (
-                <div key={i} onClick={() => setCurrentProduct(product.product_id)}>
-                    <img src={product.image.link} alt={product.image.alt} title={product.image.title}/>
-                    <p>{product.product_name}</p>
-                    <p>{product.currency.replace(product.currency, '\u00A3')}<span>{product.price}</span></p>
-                </div>
-            ) : '')}
+            <div className='recommended-container'>
+                {recommendations.hits.map((product, i) => product.image ? (
+                    <div key={i} onClick={() => setCurrentProduct(product.product_id)}>
+                        <img src={product.image.link} alt={product.image.alt} title={product.image.title}
+                             className='recommended-image'/>
+                        <p className='recommended-name'>{product.product_name}</p>
+                        <p style={{"textAlign": "center"}}>
+                            {product.currency.replace(product.currency, '\u00A3')}
+                            <span>{product.price}</span>
+                        </p>
+                    </div>
+                ) : '')}
+            </div>
             {choosenProduct ?
                 <div className='modal' onClick={() => setChoosenProduct(null)}>
                     <div className="carousel-container">
                         <button onClick={(e) => {
-                            e.preventDefault();
-                            setImage(image === 0 ? choosenProduct.image_groups[0].images.length - 1 : image - 1)
+                            e.stopPropagation();
+                            setImage(image === 0 ? choosenProduct.image_groups[0].images.length-2 : image - 1)
                         }} className="left-button">
                             &lt;
                         </button>
                         <div className="carousel" style={{"marginLeft": `${-image * 50}vw`}}>
-                            {choosenProduct.image_groups[0].images.slice(1).map(image => {
+                            {choosenProduct.image_groups[0].images.slice(1).map((image, i) => {
                                 return (
-                                    <div className="carouselContent">
+                                    <div className="carouselContent" key={i}>
                                         <img src={image.link} alt={image.alt} className='modal-img'/>
-                                    </div> )})
+                                    </div>)
+                            })
                             }
                         </div>
-                        <button onClick={(e) =>
-                        {
-                            e.preventDefault();
-                            setImage(image === choosenProduct.image_groups[0].images.length - 1 ? 0 : image + 1)}
+                        <button onClick={(e) => {
+                            e.stopPropagation();
+                            setImage(image === choosenProduct.image_groups[0].images.length-2 ? 0 : image + 1)
+                        }
                         } className="right-button">
                             &gt;
                         </button>
@@ -86,17 +91,16 @@ function App() {
                             <span>{choosenProduct.variants[0].price}</span></p>
                         <div>
                             {choosenProduct.variation_attributes.map(({id, name, values}) => (
-                                <div>
-                                    <div>{values.map(({name, value}) => {
-                                            if (id === 'color') {
-                                                return <Color value={value} image_groups={choosenProduct.image_groups}/>
-                                            } else if (id === 'size') {
-                                                return <Size name={name}/>
-                                            } else {
-                                                return '';
-                                            }
+                                <div className='modal-size'>
+                                    {values.map(({name, value}) => {
+                                        if (id === 'color') {
+                                            return <Color value={value} image_groups={choosenProduct.image_groups}/>
+                                        } else if (id === 'size') {
+                                            return <Size name={name}/>
+                                        } else {
+                                            return '';
                                         }
-                                    )}</div>
+                                    })}
                                 </div>
                             ))}
                         </div>

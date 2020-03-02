8a0a68bb69ef530e5d5061380e91babc28008a21
Max Cramer <maximiliancramer@yahoo.com>
2020-03-02 18:57:29 +0000

    trying to get images to show, nothing coming through, horrible json file.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-let data = "./data/recommendations.json";
+const data = "./data/recommendations.json";
 
 fetch(data)
   .then(res => res.json())
   .then(data => {
-    console.log(data);
+    document.getElementById("app").innerHTML = `
+        <h1 class="app-title">WE RECOMMEND</h1>
+        ${data.hits.map( function(hits) {
+            return `
+                <div class="rec__item">
+                    <img class="item__photo" src="${data.hits[9].image.link}" alt="${data.hit.image.alt}">
+                </div>
+            `
+        } ).join('')}
+    `;
+        console.log(data);
   })
   .catch(err => console.error(err));
+

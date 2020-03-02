264f7dc5ea27ee7fb186bc33faf4048b3719ac1e
Max Cramer <maximiliancramer@yahoo.com>
2020-03-02 19:27:06 +0000

    images finally coming throuhg after a good 3 hours of work that not even being paid for.

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const data = "./data/recommendations.json";
 
 fetch(data)
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("app").innerHTML = `
-        <h1 class="app-title">WE RECOMMEND</h1>
-        ${data.hits.map( function(hits) {
-            return `
-                <div class="rec__item">
-                    <img class="item__photo" src="${data.hits[9].image.link}" alt="${data.hit.image.alt}">
-                </div>
-            `
-        } ).join('')}
-    `;
-        console.log(data);
-  })
-  .catch(err => console.error(err));
+    .then(res => res.json())
+    .then(data => {
+        let html = '';
+        html += '<h1 class="app-title">WE RECOMMEND</h1>'
+        html += data.hits.map(hit => {
+            let hitHtml = '<div class="rec__item">';
+            if(!hit.image) hit.image = { link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_avaliable.svg/1200px-No_image_avaliable.svg.png' }
+            hitHtml += `<img class="item__photo" src="${hit.image.link}" alt=${hit.image.alt}></img>`
+            return hitHtml
+        } ).join('');
+        document.getElementById("app").innerHTML = html;
+    })
 
+    .catch(err => console.error(err));

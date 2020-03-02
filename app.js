5d45968fda3d154f365b8f9cf546e4fbbcd6e289
Max Cramer <maximiliancramer@yahoo.com>
2020-03-02 20:22:14 +0000

    if no image nothing shown

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const data = "./data/recommendations.json";
+const renderHit = hit => {
+  // provide a default image
+  if (!hit.image) {
+      return '';
+  }
 
-fetch(data)
-    .then(res => res.json())
-    .then(data => {
-        let html = '';
-        html += '<h1 class="app-title">WE RECOMMEND</h1>'
-        html += data.hits.map(hit => {
-            let hitHtml = '<div class="rec__item">';
-            if(!hit.image) hit.image = { link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_avaliable.svg/1200px-No_image_avaliable.svg.png' }
-            hitHtml += `<img class="item__photo" src="${hit.image.link}" alt=${hit.image.alt}></img>`
-            return hitHtml
-        } ).join('');
-        document.getElementById("app").innerHTML = html;
-    })
+  let html = '<div class="rec__item">';
+  html += `<img class="item__photo" src="${hit.image.link}" alt="${hit.image.alt}"></img>`;
+  html += "</div>";
+  return html;
+};
 
-    .catch(err => console.error(err));
+const renderComponent = hits => {
+  let html = '<div class="rec">';
+  html += '<h1 class="app-title">WE RECOMMEND</h1>';
+  hits.forEach(hit => {
+    html += renderHit(hit);
+  });
+  html += "</div>";
+  return html;
+};
+
+const setup = async () => {
+  const dataStream = await fetch("./data/recommendations.json");
+  const { hits } = await dataStream.json();
+  const html = renderComponent(hits);
+  document.getElementById("app").innerHTML = html;
+};
+
+setup();
